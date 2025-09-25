a6bec71ce67737a55fb89a2ca116811a4b25daee
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-09-25 17:34:37 -0600

    Created page component for week-3 shopping list, and added Tailwind styling

---
+++ app/week-3/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ItemList from "./item-list";
+
+const Page = () => {
+    return (
+      <main className="min-h-screen bg-black p-6">
+        <h1 className="text-3xl font-bold text-center text-white mb-6">
+            Shopping List
+        </h1>
+        <ItemList />
+      </main>
+    );
+};
+
+export default Page;
