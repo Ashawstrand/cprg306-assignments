7d9d16d720394a5d5b6306cd99c8fcaa51819d0c
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-09-25 17:58:14 -0600

    Updated styling to white text and black background

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Item = ({name, quantity, category}) => {
     return (
-      <li className="bg-white shadow-md rounded-md p-4 mb-2 hover:bg-gray-50">
-        <div className="text-lg font-semibold text-gray-800">{name}</div>
-        <div className="mt-1 text-sm text-gray-600">Quantity: {quantity}</div>
-        <div className="text-sm text-gray-600">Category: {category}</div>
+      <li className="bg-black border border-white rounded-md p-4 mb-2">
+        <div className="text-lg font-semibold text-white">{name}</div>
+        <div className="mt-1 text-sm text-white">Quantity: {quantity}</div>
+        <div className="text-sm text-white">Category: {category}</div>
       </li>
     );
 };
