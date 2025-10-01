410793f833c67829ed29fb8f718809d98be4dde0
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-10-01 12:03:20 -0600

    Added increment and decrement to new-item and added Tailwind styling for what's shown on the page

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,3 +1,26 @@
 'use client';
 import { useState } from "react";
 
+export default function NewItem() {
+
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        setQuantity((prev) => Math.min(prev + 1, 20));
+    };
+
+    const decrement = () => {
+        setQuantity((prev) => Math.max(prev - 1, 1));
+    }
+
+    return (
+        <div className="bg-white mx-auto max-w-sm mt-16 p-2 rounded">
+            <p className="text-gray-800 font-bold text-xl text-center m-4">Quantity: {quantity}</p>
+            <div className="flex justify-center">
+            <button className="bg-gray-800 m-3 text-xl text-white h-12 w-12 rounded hover:bg-gray-500" onClick={decrement}>-</button>
+            <button className="bg-blue-600 m-3 text-xl text-white h-12 w-12 rounded hover:bg-blue-500" onClick={increment}>+</button>
+            </div>
+            <p className="text-gray-800 text-lg text-center m-4">Allowed range: 1-20</p>
+        </div>
+    );
+}
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -4,6 +4,7 @@ import NewItem from "./new-item";
 const Page = () => {
     return (
         <div>
+            <h1 className="font-bold text-center text-4xl mt-16 ">Add New Item</h1>
             <NewItem />
         </div>
     );
