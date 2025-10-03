89592179213d2e85036332e90b78c557433404de
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-10-03 17:39:35 -0600

    updated Tailwind styling colour to purple  for - button

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -17,7 +17,7 @@ export default function NewItem() {
         <div className="bg-white mx-auto max-w-sm mt-16 p-2 rounded">
             <p className="text-gray-800 font-bold text-xl text-center m-4">Quantity: {quantity}</p>
             <div className="flex justify-center">
-            <button className="bg-gray-800 m-3 text-xl text-white h-12 w-12 rounded hover:bg-gray-500" onClick={decrement}>-</button>
+            <button className="bg-purple-600 m-3 text-xl text-white h-12 w-12 rounded hover:bg-purple-400" onClick={decrement}>-</button>
             <button className="bg-blue-600 m-3 text-xl text-white h-12 w-12 rounded hover:bg-blue-500" onClick={increment}>+</button>
             </div>
             <p className="text-gray-800 text-lg text-center m-4">Allowed range: 1-20</p>
