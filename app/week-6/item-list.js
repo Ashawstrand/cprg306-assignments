468be7962852e347f5f0dc84c5d56a9d78f0d978
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-10-15 21:17:17 -0600

    sort function to sort the items based on name or category

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -6,6 +6,17 @@ const ItemList = () => {
 const [itemList, setItemList] = useState(items);
 const [sortBy, setSortBy] = useState("name");
 
+//sorted copy of items array.
+const sortedItems = [...items].sort((a, b) => {
+  if (sortBy === "name") {
+    return a.name.localeCompare(b.name);
+  }
+  else if (sortBy === "category") {
+    return a.category.localeCompare(b.category);
+  }
+  return 0;
+});
+
 
 return (
     <ul className="max-w-md mx-auto mt-6">
