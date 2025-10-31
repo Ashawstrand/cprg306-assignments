f15d9127bc9896fad7826c745fac27a0e308ff25
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-10-30 22:52:34 -0600

    updated item.js to add additional prop of onSelect and onClick

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Item = ({name, quantity, category}) => {
+const Item = ({name, quantity, category, onSelect}) => {
     return (
-      <li className="bg-black border border-white rounded-md p-4 mb-2">
+      <li className="bg-black border border-white rounded-md p-4 mb-2"
+          onClick={() => onSelect && onSelect({name, quantity, category})}
+      >
         <div className="text-lg font-semibold text-white">{name}</div>
         <div className="mt-1 text-sm text-white">Quantity: {quantity}</div>
         <div className="text-sm text-white">Category: {category}</div>
