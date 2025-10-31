e1e92f2e6d7126135c04e6ca29f2583d92ca0c71
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-10-30 22:23:52 -0600

    Remove accidentally committed .next folder

--- app/.next/static/chunks/cprg306-assignments_app_layout_14588e54.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d16df498._.css"
-],
-    source: "dynamic"
-});
--- app/.next/static/chunks/cprg306-assignments_app_week-2_page_bb352678.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/cprg306-assignments_11055223._.js"
-],
-    source: "dynamic"
-});
