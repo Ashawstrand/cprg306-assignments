966307d09a6ffc97b60b4f85b72d4254e98f4039
Ashawstrand <Ashley.Shaw-Strand@edu.sait.ca>
2025-11-10 15:12:44 -0700

    created contexts folder, AuthContext.js, and added AuthContext.Provider to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "../contexts/AuthContext.js";
+ 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AuthContextProvider>{children}</AuthContextProvider>
         {children}
       </body>
     </html>
   );
-}
+} 
